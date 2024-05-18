91b4e7e78f31500b5f328af7117aedfff3d7cb74
Roisin-M <S00223865@atu.ie>
2024-05-18 10:31:56 +0100

    displaying apidata

--- src/app/components/displaydetails/displaydetails.component.ts
+++ src/app/components/displaydetails/displaydetails.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MockapiService } from '../../services/mockapi.service';
+import { companyDetails } from '../../interfaces/interfacecomponent';
 
 @Component({
   selector: 'app-displaydetails',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './displaydetails.component.html',
   styleUrl: './displaydetails.component.css'
 })
-export class DisplaydetailsComponent {
+export class DisplaydetailsComponent implements OnInit {
 
+  constructor(private _mockapi:MockapiService){}
+  results:companyDetails[]=[];
+  //invoke getCompanies
+  ngOnInit(): void {
+    this.getCompanies();
+  }
+  //use service to get companydata
+  getCompanies(): boolean {
+    this._mockapi.getCompanyDetails().subscribe(
+      companyData => {
+        this.results=companyData;
+        console.log(this.results);
+      }
+    )
+    return false;
+  }
 }
