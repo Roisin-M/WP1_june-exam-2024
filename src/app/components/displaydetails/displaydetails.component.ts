35863f3b0719db935749f6ac90ec358992cd56ea
Roisin-M <S00223865@atu.ie>
2024-05-18 09:48:41 +0100

    bootstrap

---
+++ src/app/components/displaydetails/displaydetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displaydetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './displaydetails.component.html',
+  styleUrl: './displaydetails.component.css'
+})
+export class DisplaydetailsComponent {
+
+}
