b59639f6bc4a77b6abb61c03daa31698a975340d
Roisin-M <S00223865@atu.ie>
2024-05-18 09:57:46 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DisplaydetailsComponent } from './components/displaydetails/displaydetails.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:DisplaydetailsComponent},
+    {path:'about',component:AboutComponent}
+];
